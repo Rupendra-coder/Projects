6c3da6ec02ebfba1b835e2142ae2725c456b42f7
Rupendra-coder <magarrupendra@gmail.com>
2019-11-19 07:43:18 -0800

    This is function

---
+++ Function.py
@@ -0,0 +1,11 @@
+a=[[1,2,3],[4,5,6]]
+for i in range(len(a)):
+    for j in range (len(a[i])):
+        print(a[i],[j])
+
+# Iterating through 2D lists
+
+a=[[1,2,3],[4,5,6]]
+for row in a:
+    for element in row:
+        print(element)
